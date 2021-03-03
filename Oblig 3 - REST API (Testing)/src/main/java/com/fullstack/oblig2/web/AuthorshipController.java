d18a1dfc97b2c7b3900266c52378dbdaea4a4eaf
Eirik Steira <eirsteir@stud.ntnu.no>
2021-03-03 15:10:40 +0100

    Oblig 3

--- Oblig 3 - REST API (Testing)/src/main/java/com/fullstack/oblig2/web/AuthorshipController.java
+++ Oblig 3 - REST API (Testing)/src/main/java/com/fullstack/oblig2/web/AuthorshipController.java
@@ -11,7 +11,7 @@ import java.util.List;
 
 @Slf4j
 @RestController
-@RequestMapping("/authors/{authorId}/books/")
+@RequestMapping("/authors/{authorId}/books")
 public class AuthorshipController {
 
     @Autowired
@@ -22,9 +22,9 @@ public class AuthorshipController {
         return authorshipService.getBooksByAuthor(authorId);
     }
 
-    @GetMapping
-    public List<Book> searchBooks(@PathVariable Long authorId, @RequestParam String query) {
-        return authorshipService.searchBooksByAuthor(authorId, query);
-    }
+//    @GetMapping
+//    public List<Book> searchBooks(@PathVariable Long authorId, @RequestParam String query) {
+//        return authorshipService.searchBooksByAuthor(authorId, query);
+//    }
 
 }
